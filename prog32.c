869736806f3e885e461a4c1a3765d6d228aeb60a
poisontwist111 <u24ai099@aid.svnit.ac.in>
2024-09-10 20:40:06 +0530

    More Debugging

--- prog32.c
+++ prog32.c
@@ -14,18 +14,18 @@ int main()
    printf("Enter the numbers : \n");
    scanf("%d",&n);
    if (max1>=n) {
-   max1 = max1;
-   if (max2>=n) {
-   max2 = max2;
+         max1 = max1;
+        if (max2>=n) {
+             max2 = max2;
+         } else {
+         max2 = n;
+         }
    } else {
-   max2 = n;
-   }
-   } else {
-   max2 = max1;
-   max1 = n;
+         max2 = max1;
+         max1 = n;
    }
    printf("first maximum = %d\n",max1);
-   printf("first minimum = %d\n",max2);
+   printf("second maximum = %d\n",max2);
    }
    return 0;
 }
