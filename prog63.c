edc7d39fb123085007994355c62e59f4fedb3cd0
poisontwist111 <u24ai099@aid.svnit.ac.in>
2024-09-14 15:14:04 +0530

    Lab 5 and Lab 6

---
+++ prog63.c
@@ -0,0 +1,10 @@
+// PROGRAM TO READ A 3*3 MATRIX AND FINDOUT MAX AND MIN ELEMENT.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num[3][3];
+    int max1,max2,max3,ult_max;
+   
+    return 0;
+}
